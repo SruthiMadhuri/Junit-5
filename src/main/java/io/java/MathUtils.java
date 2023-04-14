24f901aa721bdc2f90a2759500d591e607e047f5
2115022 <2115022@LTIN293770.cts.com>
2023-04-14 17:01:28 +0530

    Asserting exceptions with assertThrows

--- src/main/java/io/java/MathUtils.java
+++ src/main/java/io/java/MathUtils.java
@@ -2,7 +2,19 @@ package io.java;
 
 public class MathUtils {
 	public int add(int a ,int b) {
-		return a+b;
+		return a+b;	
+	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a /  b;
+	}
+	public double computeCircleAreaw(double radius) {
+		return Math.PI * radius * radius;
 		
 	}
 
--- src/test/java/io/java/MathUtilsTest.java
+++ src/test/java/io/java/MathUtilsTest.java
@@ -1,17 +1,25 @@
 package io.java;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mathUtils=new MathUtils();
 		int expected=2;
 		int actual=mathUtils.add(1, 1);
 		assertEquals(expected, actual,"The add method should be two numbers");
+	}
+	@Test
+	void testDivide() {
+		MathUtils mathUtils=new MathUtils();
+		assertThrows(ArithmeticException.class, () -> mathUtils.div(1, 0),"Divide by zero should throw");
+		
+	}
+	@Test
+	void testComputeCircleadius() {
+		MathUtils mathUtils=new MathUtils();
+		assertEquals(314.1592653589793,mathUtils.computeCircleAreaw(10),"Should return right circle area");
 	}
 
 }
