047a6a7f83ea8bbc380c34b8b2fd2533ce156235
2115022 <2115022@LTIN293770.cts.com>
2023-04-14 14:29:08 +0530

    Assertions

---
+++ src/main/java/io/java/MathUtils.java
@@ -0,0 +1,9 @@
+package io.java;
+
+public class MathUtils {
+	public int add(int a ,int b) {
+		return a+b;
+		
+	}
+
+}
---
+++ src/test/java/io/java/MathUtilsTest.java
@@ -0,0 +1,17 @@
+package io.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		MathUtils mathUtils=new MathUtils();
+		int expected=2;
+		int actual=mathUtils.add(1, 1);
+		assertEquals(expected, actual,"The add method should be two numbers");
+	}
+
+}
